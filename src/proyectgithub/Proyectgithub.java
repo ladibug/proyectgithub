efb0338b9172f05b2e2d35107154ddce64046b88
michaeltoledovillegas <michaeltoledovillegas@10.34.13.67>
2015-12-03 19:59:12 -0300

    infinitecimo intento parte 3

--- src/proyectgithub/Proyectgithub.java
+++ src/proyectgithub/Proyectgithub.java
@@ -11,11 +11,9 @@ package proyectgithub;
  */
 public class Proyectgithub {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("hola");
     }
     
 }
